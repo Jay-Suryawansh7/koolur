a0ac92c63b9b9b5b99662c433c0482fe17494b2c
Jay-Suryawansh7 <143527615+Jay-Suryawansh7@users.noreply.github.com>
2026-04-06 17:02:14 +0530

    fix: bump to 0.1.3 and add integrity hash

--- src/integrity.ts
+++ src/integrity.ts
@@ -6,6 +6,7 @@
 
 export const INTEGRITY_CHECKSUM = {
   'index.js': 'dde65f58d7847109fca75ba71f0a21b997ade0beaf7a638dfa461231899342db',
+  'integrity.js': '62263c8485f7e985d773ea7ffb1ec001ab484def10fbff9cb992e63528d3d26f',
   'browser.js': '3ab88d13f375af3eb4cf2a43d3cb314bf31933ed28f2da7a0a67560f9c101add',
   'json.js': 'cb9b6d8229bf445db6bb4b21ada041c4e816cfb158a50d3016a9b8d945bf29d2',
   'terminal.js': '980108c970524a77f1520c941ed408cc0d54909cd9f2dd5cce0de8f48f07b194',
